ea3fb5a2c9edd34547a00b3213a7bd86eff4eee7
rswasthik <swasthik6083@gmail.com>
2023-11-24 13:58:52 +0530

    message

---
+++ lib/features/admin/widgets/category_products_chart.dart
@@ -0,0 +1,16 @@
+import 'package:amazonapp/features/admin/models/sales.dart';
+import 'package:flutter/material.dart';
+
+// class CategoryProductsChart extends StatelessWidget {
+//   const CategoryProductsChart({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return charts.BarChart(
+//       seriesList,
+//       animate: true,
+//     );
+//   }
+// }
---
+++ lib/features/search/services/search_services.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:amazonapp/constants/error_handling.dart';
+import 'package:amazonapp/constants/global_variables.dart';
+import 'package:amazonapp/constants/utils.dart';
+import 'package:amazonapp/models/product.dart';
+import 'package:amazonapp/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+class SearchServices {
+  Future<List<Product>> fetchSearchedProduct({
+    required BuildContext context,
+    required String searchQuery,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Product> productList = [];
+    try {
+      http.Response res = await http.get(
+        Uri.parse('$uri/api/products/search/$searchQuery'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(res.body).length; i++) {
+            productList.add(
+              Product.fromJson(
+                jsonEncode(
+                  jsonDecode(res.body)[i],
+                ),
+              ),
+            );
+          }
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return productList;
+  }
+}
